1255b7a54c349adf5a937290926cd1dd7458c469
PamanAleph <alieffirmandabisnis@gmail.com>
2024-10-06 08:21:36 +0700

    fix layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,70 +4,99 @@
   import SahariLogo from "../assets/logo/sahari.png";
   import Button from "../components/Button.svelte";
   import GridBeam from "../components/GridBeam.svelte";
+  import BoxReveal from "../components/BoxReveal.svelte";
+
+  // mapping
+  const ServiceCard = [
+    {
+      title: 'Sahari Friends<span class="text-[#5046e6]">.</span>',
+      description: `
+        Sahari Friends offers companionship for users engaging in social activities, 
+        such as outings or visits to specific locations. With professional talents 
+        chosen by the user, you can enjoy a safe and comfortable social experience 
+        without the risk of negative exploitation.
+      `,
+    },
+    {
+      title: 'Sahari Memori',
+      description: `
+        Sahari Memori allows users to interact with an AI that mimics the responses 
+        of a deceased loved one. By using memory and voice samples, this AI creates 
+        a warm and personal conversation experience, helping relive cherished moments.
+      `,
+    },
+    {
+      title: 'Sahari Memory<span class="text-[#5046e6]">+</span>',
+      description: `
+        A premium service that offers additional features such as priority bookings 
+        and more flexible interaction durations. Enjoy a richer experience with enhanced 
+        access to Sahari Memori, making your memories even more special.
+      `,
+    },
+  ];
 </script>
 
-<section>
-  <div class="relative flex items-center">
-    <div class="py-6 px-10 md:px-40">
-      <BlurFade delay={0.75}>
-        <h1
-          class="text-[3rem] md:text-[4rem] w-full text-left md:w-[50rem] font-bold px-4 md:px-0"
-        >
-          Connect, Share, and Thrive with
-        </h1>
-      </BlurFade>
-      <BlurFade delay={1}>
-        <p
-          class="text-[3rem] md:text-[4rem] font-bold bg-gradient-to-r from-violet-600 to-orange-400 text-transparent bg-clip-text"
-        >
-          SAHARI <span
-            class="bg-gradient-to-r text-transparent bg-clip-text from-violet-600 to-orange-400"
-            >APP</span
-          >
-        </p>
-      </BlurFade>
-      <BlurFade delay={1.25}>
-        <h2
-          class="py-4 md:py-10 text-center md:text-justify text-lg md:text-xl w-full md:w-[40rem] px-4 md:px-0"
-        >
-          We are Sahari, your trusted platform for meaningful connections.
-          Offering safe companionship and AI-driven experiences, we’re committed
-          to helping you feel connected and supported.
-        </h2>
-        <Button class="my-4">Join Us</Button>
-      </BlurFade>
-    </div>
-    <BlurFade delay={1.25}>
-      <img
-        src={SahariLogo}
-        alt="Logo"
-        class="h-[20rem] drop-shadow-[0_10px_10px_#AE70FE] bounce-and-sway-animation"
-      />
-    </BlurFade>
-    <Particles className="absolute inset-0" refresh={true} />
-  </div>
-
-  <div
-  class="w-full h-[400px] dark:bg-grid-white/[0.05] bg-grid-black/[0.07]"
->
-  <GridBeam class="sm:pl-16 pt-28 pl-4 flex items-start justify-start">
-    <div class="grid gap-2">
-      <h1 class="sm:text-5xl text-4xl font-bold max-w-sm">
-        Hello Amazing Svelte Developer
+<section class="relative flex items-center">
+  <div class="py-10 px-10 md:px-40 md:text-left text-center">
+    <BlurFade delay={0.5}>
+      <h1
+        class="text-[2rem] md:text-[4rem] w-full md:w-[50rem] font-bold px-4 md:px-0"
+      >
+        Connect, Share, and Thrive with
       </h1>
-      <p class="text-neutral-500 max-w-lg">
-        Beautiful beam which I btw reverse engineered from <span
-          class="text-primary">Aceternity UI</span
+    </BlurFade>
+    <BlurFade delay={0.65}>
+      <p
+        class="text-[2rem] md:text-[4rem] font-bold bg-gradient-to-r from-violet-600 to-orange-400 text-transparent bg-clip-text"
+      >
+        SAHARI <span
+          class="bg-gradient-to-r text-transparent bg-clip-text from-violet-600 to-orange-400"
+          >APP</span
         >
       </p>
-      <p class="text-neutral-500 max-w-lg">
-        Just Copied it from : <a
-          href="https://ui.lukacho.com/components/background-grid-beam"
-          target="_blank"
-          class="text-primary">Lukacho UI</a
-        >
-      </p>
-    </div>
-  </GridBeam>
-</div>
+    </BlurFade>
+    <BlurFade delay={0.8}>
+      <h2
+        class="py-4 md:py-10 text-justify text-md md:text-xl w-full md:w-[40rem] px-4 md:px-0"
+      >
+        We are Sahari, your trusted platform for meaningful connections.
+        Offering safe companionship and AI-driven experiences, we’re committed
+        to helping you feel connected and supported.
+      </h2>
+      <div class="flex justify-center md:justify-start">
+
+        <Button class="my-4 ">Join Us</Button>
+      </div>
+    </BlurFade>
+  </div>
+  <BlurFade delay={0.8}>
+    <img
+      src={SahariLogo}
+      alt="Logo"
+      class="h-[20rem] drop-shadow-[0_10px_10px_#AE70FE] bounce-and-sway-animation hidden md:block"
+    />
+  </BlurFade>
+  <Particles className="absolute inset-0" refresh={true} />
 </section>
+
+<GridBeam class="bg-grid-white/[0.05]">
+  <div class="w-full py-6 px-10 md:px-40">
+    <h1 class="text-center text-4xl font-bold py-6">Our Services</h1>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-10">
+      {#each ServiceCard as card}
+        <div class="mb-8 p-6 rounded-2xl shadow-md shadow-[#AE70FE] ">
+          <BoxReveal boxColor="#5046e6" duration={0.5}>
+            <p class="text-[2rem] font-semibold">
+              {@html card.title}
+            </p>
+          </BoxReveal>
+          <BoxReveal boxColor="#5046e6" duration={0.5}>
+            <h2 class="mt-[.5rem] text-[1rem] text-justify">
+              {@html card.description}
+            </h2>
+          </BoxReveal>
+        </div>
+      {/each}
+    </div>
+  </div>
+</GridBeam>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,43 @@
+const plugin = require('tailwindcss/plugin');
+const flattenColorPalette = require('tailwindcss/lib/util/flattenColorPalette').default;
+const svgToDataUri = require('mini-svg-data-uri');
+
+const addVariablesForColors = plugin(function({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme('colors'));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ':root': newVars
+  });
+});
+
+const addGridAndDotPatterns = plugin(function({ matchUtilities, theme }) {
+  matchUtilities(
+    {
+      "bg-grid": (value) => ({
+        backgroundImage: `url("${svgToDataUri(
+          `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+        )}")`,
+      }),
+      "bg-grid-small": (value) => ({
+        backgroundImage: `url("${svgToDataUri(
+          `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+        )}")`,
+      }),
+      "bg-dot": (value) => ({
+        backgroundImage: `url("${svgToDataUri(
+          `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+        )}")`,
+      }),
+    },
+    { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+  );
+});
+
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
@@ -42,39 +80,7 @@ export default {
     },
   },
   plugins: [
-    addVariablesForColors,  
-    function ({ matchUtilities, theme }) {
-      matchUtilities(
-        {
-          "bg-grid": (value) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-            )}")`,
-          }),
-          "bg-grid-small": (value) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-            )}")`,
-          }),
-          "bg-dot": (value) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
-            )}")`,
-          }),
-        },
-        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
-      );
-    },
+    addVariablesForColors,
+    addGridAndDotPatterns,
   ],
 };
-
-function addVariablesForColors({ addBase, theme }) {
-  let allColors = flattenColorPalette(theme("colors"));
-  let newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-  );
-
-  addBase({
-    ":root": newVars,
-  });
-}
